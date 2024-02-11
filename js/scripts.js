7a3186a4e40bf4138d0e5409a630e0b29714f18b
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-02-11 16:59:57 +0100

    Update Food

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,6 @@
-
-// Let zum Speichern / Deklarieren von Variablen
-// let age = 29;
-
-// let myName = 'Tobias';
-// document.write(myName);
-// myName = 'Tobsen Noah';
-// document.write(myName);
-
-
-// let simpleAddition = 2 + 6;
-// document.write(simpleAddition);
-
-// let size = 100;
-// let doubleSize = size * 2;
-// document.write(doubleSize);
-
-// let size = 100;
-// let doubleSize = size * 2;
-// let minSize = (doubleSize * 2) - (size / 2);
-// document.write(minSize);
-
 alert('Hello World');
 
-let favoriteInternationalFood = 'Pad Thai &';
-let favoriteLocalFood = ' Zwiebelrostbraten';
+let favoriteInternationalFood = 'International: Pad Thai / ';
+let favoriteLocalFood = ' Local: Zwiebelrostbraten';
 document.write(favoriteInternationalFood);
 document.write(favoriteLocalFood);
-
-
