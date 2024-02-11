f195c70dd983f790bfc25d90e5ac567568927b2a
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-02-11 15:47:51 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// alert('Hello world');
+// Let zum Speichern / Deklarieren von Variablen
+// let age = 29;
+
+// let myName = 'Tobias';
+// document.write(myName);
+// myName = 'Tobsen Noah';
+// document.write(myName);
+
+
+// let simpleAddition = 2 + 6;
+// document.write(simpleAddition);
+
+// let size = 100;
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
