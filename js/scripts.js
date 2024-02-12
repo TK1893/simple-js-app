770dd5b1f167a87b3e131ade2df563aa959246ba
Tobias Kraft <155923508+TK1893@users.noreply.github.com>
2024-02-12 17:59:07 +0100

    Set up from scratch

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
-alert('Hello World');
+// ---- Task Version ------------------------------------------------------------------  Task Version  ----
 
-let favoriteInternationalFood = 'International: Pad Thai / ';
-let favoriteLocalFood = ' Local: Zwiebelrostbraten';
-document.write(favoriteInternationalFood);
-document.write(favoriteLocalFood);
+// 1. Blank array
+let pokemonList = []
+
+// 2. Add objects to the array with at least 3 keys + values
+pokemonList = [
+  { ranking: 250, name: 'Ho-oh', height: 3.80, type: ['Fire', 'Flying'] },
+  { ranking: 376, name: 'Metagross', height: 1.60, type: ['Psychic', 'Steel'] },
+  { ranking: 380, name: 'Latias', height: 1.40, type: ['Psychic', 'Dragon'] },
+  { ranking: 382, name: 'Kyogre', height: 4.50, type: ['Water'] },
+  { ranking: 647, name: 'Keldeo', height: 1.40, type: ['Water', 'Fighting'] }
+];
+
+// 3. Check
+// document.write(pokemonList[0].ranking, pokemonList[2].height, pokemonList[4].type[1]);
+document.write(pokemonList[4].ranking, pokemonList[4].name);
